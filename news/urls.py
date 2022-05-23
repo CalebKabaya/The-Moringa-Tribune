70cbe063b10aa59345e375ed8911c03839981063
calebkabaya <calebkabaya@gmail.com>
2022-05-23 22:59:36 +0300

    added the navbar

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+# from django.conf.urls import url
+from django.urls import re_path
+
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name='welcome'),
-    url('^today/$',views.news_of_day,name='newsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    re_path('^$',views.welcome,name='welcome'),
+    re_path('^today/$',views.news_of_day,name='newsToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
  
 
     ]
