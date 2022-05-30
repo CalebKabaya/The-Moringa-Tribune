9f7055475d04a69701b81ad1b51ac0b99fc975c5
Moringa School <calebmutuku4@gmail.com>
2022-05-30 13:45:15 +0300

    ft:cinfigured the all news url

--- news/urls.py
+++ news/urls.py
@@ -6,8 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.news_all,name='allNews'),
-    # re_path('^$',views.news_today,name='newsToday'),
-
+    re_path(r'^todaysnews/',views.news_today,name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     re_path(r'^search/', views.search_results, name='search_results'),
     re_path(r'^article/(\d+)',views.article,name ='article')
