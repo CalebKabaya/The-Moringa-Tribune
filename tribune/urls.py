3bf29c3596d92951bc2ba2a9223eda5f209f81f1
Moringa School <calebmutuku4@gmail.com>
2022-06-01 09:21:52 +0300

    aded the configuration for logout

--- tribune/urls.py
+++ tribune/urls.py
@@ -21,13 +21,16 @@ from django.conf.urls import url
 
 
 
+from django.contrib.auth import views 
 
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
-   url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+
 
 
 ]
